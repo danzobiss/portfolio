d82bc8eab6b13593d3f8f31069d75e35e626b1d3
danzobiss <danzobiss@gmail.com>
2022-07-17 20:16:14 -0300

    ScrollReveal Animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,3 +48,21 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+
+/*========================== SCROLL REVEAL ANIMATION =========================*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+})
+
+sr.reveal('.profile .border')
+sr.reveal('.profile .name', {delay: 500})
+sr.reveal('.profile .profession', {delay: 600})
+sr.reveal('.profile .social', {delay: 700})
+sr.reveal('.profile .info-group', {interval: 100, delay: 700})
+sr.reveal('.profile .buttons', {delay: 800})
+sr.reveal('.filters .content', {delay: 900})
+sr.reveal('.filters', {delay: 1000})
